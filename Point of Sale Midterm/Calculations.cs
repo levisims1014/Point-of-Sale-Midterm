fa0d1e082b3fc1a20d979d7b640780eef0e8c999
mattlaura <lhuiefiction@gmail.com>
2019-02-04 09:17:44 -0500

    calculation class updated

--- Point of Sale Midterm/Calculations.cs
+++ Point of Sale Midterm/Calculations.cs
@@ -9,28 +9,35 @@ namespace Point_of_Sale_Midterm
     class Calculations
     {
 
-        double itemTotal = 0;
+        double subtotal;
+        double salesTax;
+        double grandTotal;
         List<orderedItemInfo> orderedItems = new List<orderedItemInfo>();
         public Calculations(List<orderedItemInfo> orderedItems)
         {
             this.orderedItems = orderedItems;
+            Totals();
         }
-        //ItemInformation(string name, string category, string description, double price
-        public void Totals()
+        
+        public void Totals() //change to public static int Totals (pass in list) return 
         {
             List<double> costs = new List<double>();
-            double itemTotal = 0;
-            double subtotal = 0;
-            double salesTax = 0;
-            double grandTotal = 0;
-            double Price;
-            int Quantity;
+            subtotal = 0;
+            salesTax = 0.06; //Michigan sales tax is 6%.
+            grandTotal = 0;
+
             foreach (orderedItemInfo item in orderedItems)
             {
                 subtotal = item.Price * item.Quantity;
+                Console.WriteLine(item.Name + "\t" + "Quantity: " + item.Quantity + "\t" + subtotal);//transfer to receipt view for later
                 costs.Add(subtotal);
             }
-            Console.WriteLine(costs.Sum());
+            double sum = costs.Sum();
+            Console.WriteLine("Subtotal = " + sum);//transfer to receipt view
+            salesTax = costs.Sum() * salesTax;
+            grandTotal = Math.Round(salesTax + sum, 2);
+            Console.WriteLine("Sales Tax = " + salesTax);//transfer to receipt view
+            Console.WriteLine("Total = " + grandTotal);//transfer to receipt view
 
 
         }
